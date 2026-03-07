d38ea4bf1687db954abeb65da16e01d209634676
Mochtar Chia <mochamoc@gmail.com>
2026-03-08 10:58:26 +1100

    Simplify Clerk middleware

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,7 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher([
-  "/",
-  "/sign-in(.*)",
-  "/sign-up(.*)",
-  "/coming-soon(.*)",
-  "/api/early-access(.*)",
-]);
-
-export default clerkMiddleware((auth, req) => {
-  if (!isPublicRoute(req)) auth.protect();
-});
+export default clerkMiddleware();
 
 export const config = {
-  matcher: [
-    "/((?!.*\\..*|_next).*)",
-    "/",
-    "/(api|trpc)(.*)",
-  ],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
