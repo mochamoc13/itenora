616f5d195cd0bc98971c53f22c84a8e5f852477d
Mochtar Chia <mochamoc@gmail.com>
2026-04-02 23:36:00 +1100

    Remove redirects to restore site

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "www.itenora.com",
-          },
-        ],
-        destination: "https://itenora.com/:path*",
-        permanent: true,
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
