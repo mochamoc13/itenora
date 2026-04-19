801ad0833153ac1c0810d50f4cbb643a0b4d8a60
Mochtar Chia <mochamoc@gmail.com>
2026-04-19 11:34:12 +1000

    fix www redirect for SEO

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.itenora.com',
+          },
+        ],
+        destination: 'https://itenora.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
